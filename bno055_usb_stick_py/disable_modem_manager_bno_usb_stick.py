7dcd56ca81d45b265dd7d0abe5740ecddadd385d
Konstantin Selyunin <selyunin.k.v@gmail.com>
2018-12-28 17:31:44 +0100

    updating modem script

--- bno055_usb_stick_py/disable_modem_manager_bno_usb_stick.py
+++ bno055_usb_stick_py/disable_modem_manager_bno_usb_stick.py
@@ -18,11 +18,20 @@ def copy_rules_to_udev():
     rules_file = '97-ttyacm.rules'
     file_path = os.path.dirname(os.path.abspath(__file__))
     rules_file_abspath = os.path.join(file_path, rules_file)
+    print(f"copying rules file: {rules_file_abspath} to /etc/udev/rules.d")
     subprocess.call(['sudo', 'cp', rules_file_abspath, '/etc/udev/rules.d'])
 
 
 def reload_udev_rules():
     subprocess.call(['sudo', 'udevadm', 'control', '--reload-rules'])
+
+
+def disable_modem_manager_bno():
+    if prompt_sudo() != 0:
+        print("ERROR: User should have root privileges! script will *NOT* be executed, exit")
+        exit(1)
+    copy_rules_to_udev()
+    reload_udev_rules()
 
 
 if __name__ == '__main__':
