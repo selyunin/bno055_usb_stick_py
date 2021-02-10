3de0eb930095bbd1e41792d41d4f75e12557db89
Konstantin Selyunin <selyunin.k.v@gmail.com>
2021-02-10 20:34:30 +0100

    adding reset example of the chip

--- bno055_usb_stick_py/examples.py
+++ bno055_usb_stick_py/examples.py
@@ -42,9 +42,34 @@ def example_streaming_single():
         print(f"{packet}")
 
 
+def example_reset():
+    from time import sleep
+    bno_usb_stick = BnoUsbStick()
+    # 1. read current operation mode
+    opr_mode_addr = 0x3D
+    mode = bno_usb_stick.read_register(opr_mode_addr) & 0x0F
+    print(f"Initial operation mode: {mode:04b}")
+    # 2. change operation mode to NDOF, p.22, Table 3-5 of the datasheet
+    ndof_mode = 0b00001100
+    bno_usb_stick.write_register(opr_mode_addr, ndof_mode)
+    sleep(0.1)
+    # 3. read back the mode register, double check if mode is written
+    mode = bno_usb_stick.read_register(opr_mode_addr) & 0x0F
+    print(f"Changed operation mode: {mode:04b}")
+    # 4. set RST_SYS bit of SYS_TRIGGER register (i.e. initiate a reset)
+    sys_trigger_addr = 0x3F
+    reset_val = 1 << 5
+    bno_usb_stick.write_register(sys_trigger_addr, reset_val)
+    sleep(1.0)
+    # 5. read back the operation mode, check if chip is reset
+    mode = bno_usb_stick.read_register(opr_mode_addr) & 0x0F
+    print(f"Mode upon reset is: {mode:04b}")
+
+
 if __name__ == '__main__':
     example_register_read()
     example_register_write()
     example_burst_read()
     example_streaming_generator()
     example_streaming_single()
+    example_reset()
